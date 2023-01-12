4118bc70d7122935014f82f30f929893905d2039
Jose Scharneck <jose.scharneck87@gmail.com>
2023-01-12 13:12:27 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express');
+const pug = require('pug');
+const puppeteer = require("puppeteer");
 const app = express();
 
+app.set('view engine', 'pug');
 
-app.get('/', (req, res)=>{
+app.get('/api/v1/', async (req, res)=>{
+    
+    //console.log(req.url);
+    // let browser = await puppeteer.launch({ headless: true });
+    // let page = await browser.newPage();
+    // await page.goto("https://aa.co.za/fuel-pricing/");
 
-    res.send('<h1>hello world</h1>');
+    // setTimeout(async () => {
+    //     let grabParagraph = await page.evaluate(() => {
+    //         let pgTag = document.querySelector("div.s2.srcl p strong");            
+    //         return pgTag.innerHTML;
+    //     });
+
+    //     console.log("grabParagraph: ",grabParagraph);
+    //     price = grabParagraph;
+
+    //     console.log("the price: ", price);
+
+    //     await browser.close();
+
+    // }, 2500);
+
+    let greeting = "Hello world";
+
+    res.status(200).render('index', {greeting: greeting});
+
 });
 
 
 
 app.listen(3000, ()=>{
 
-    console.log('listening on port 3000');
+    console.log('started');
 })
